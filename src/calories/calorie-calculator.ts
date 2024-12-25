158a0b85398adeea1e793cde9512d2e6b48fdec3
DSaatz <126864831+DSaatz@users.noreply.github.com>
2024-12-25 12:22:02 +0100

    fix: fixxed wrong formatting in calorie-calculator.ts

--- src/calories/calorie-calculator.ts
+++ src/calories/calorie-calculator.ts
@@ -1,6 +1,6 @@
-import { dietGoal } from "./enums/diet-goal-enum";
+import { DietGoal } from "./enums/diet-goal-enum";
 import { sex } from "./enums/sex-enum"; //the sex enum might be not needed if I stick to the Katch-McArdle formula
-import { activityLevel } from "./enums/activity-level-enum";
+import { ActivityLevel } from "./enums/activity-level-enum";
 
 export class CalorieCalculator{
     private readonly dietSummant = 200; // Additional 200 calories to either cut or bulk slowly 
@@ -8,14 +8,14 @@ export class CalorieCalculator{
     public calculateMaintenanceCalories(
         weight: number,
         bodyFatPercentage: number,
-        activityLevel: activityLevel
+        activityLevel: ActivityLevel
     ): number{
         const leandBodyMass = weight * (1 - bodyFatPercentage / 100);
 
         //Basal Metabolic Rate using Katch-McArdle formula
         const bmr = 370 + 21.6 * leandBodyMass;
 
-        const activityMultipliers: Record<activityLevel, number> = {
+        const activityMultipliers: Record<ActivityLevel, number> = {
             sedentary: 1.2,
             lightlyActive: 1.375,
             moderatelyActive: 1.55,
@@ -30,10 +30,10 @@ export class CalorieCalculator{
 
     public calculateCaloricIntake(
         maintenanceCalories: number,
-        dietGoal: dietGoal
+        dietGoal: DietGoal
     ): number{
 
-        const calorieAdjustments: Record<dietGoal, number> = {
+        const calorieAdjustments: Record<DietGoal, number> = {
             cut: -this.dietSummant,
             maintain: 0,
             bulk: this.dietSummant,
