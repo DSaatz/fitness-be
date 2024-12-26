8f9ac1291bcd70e8295e45b3620fa25f829d3a34
DSaatz <126864831+DSaatz@users.noreply.github.com>
2024-12-26 19:18:19 +0100

    feat: set up DB connection and first API Services for users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { User, UsersSchema } from './schemas/users.schema';
+
 
 @Module({
-  controllers: [UsersController],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UsersSchema }]),
+  ],
   providers: [UsersService],
+  controllers: [UsersController],
+  exports: [UsersService],
 })
 export class UsersModule {}
