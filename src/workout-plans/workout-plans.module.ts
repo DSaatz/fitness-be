ed79c877a415769688545d56134a719525968af0
DSaatz <126864831+DSaatz@users.noreply.github.com>
2024-12-27 15:13:16 +0100

    feat: implemented basic API capabilities for Workoutplans

--- src/workout-plans/workout-plans.module.ts
+++ src/workout-plans/workout-plans.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { WorkoutPlansService } from './workout-plans.service';
 import { WorkoutPlansController } from './workout-plans.controller';
+import { WorkoutPlan, WorkoutPlansSchema } from './schemas/workout-plans.schema';
+import { Exercise, ExerciseSchema } from '../exercises/schemas/exercise.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: WorkoutPlan.name, schema: WorkoutPlansSchema },
+      { name: Exercise.name, schema: ExerciseSchema }
+    ])
+  ],
   controllers: [WorkoutPlansController],
-  providers: [WorkoutPlansService],
+  providers: [WorkoutPlansService]
 })
 export class WorkoutPlansModule {}
