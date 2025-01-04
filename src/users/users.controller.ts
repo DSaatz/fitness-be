0e4676ae9300c10f69a763fb3e3e135ae4a06b0d
DSaatz <126864831+DSaatz@users.noreply.github.com>
2025-01-04 13:36:12 +0100

    feat: added generic methods to users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   NotFoundException,
+  Put,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -29,18 +30,18 @@ export class UsersController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+      return await this.usersService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+      return await this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  async remove(@Param('id') id: string) {
+      return await this.usersService.remove(id);
   }
 
   @Post('/:userId/workout-plan')
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -20,17 +20,49 @@ export class UsersService {
     return this.userModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+
+  async findOne(id: string): Promise<User | null> {
+    if (!Types.ObjectId.isValid(id)) {
+      throw new NotFoundException('Invalid user id');
+    }
+    const user = await this.userModel.findById(id).exec();
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<User | null> {
+    if (!Types.ObjectId.isValid(id)) {
+      throw new NotFoundException('Invalid user id');
+    }
+    const updatedUser = await this.userModel
+      .findByIdAndUpdate(id, updateUserDto, { new: true }) 
+      .exec();
+
+    if (!updatedUser) {
+      throw new NotFoundException('User not found');
+    }
+
+    return updatedUser;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+  async remove(id: string): Promise<User | null> {
+    if (!Types.ObjectId.isValid(id)) {
+     throw new NotFoundException('Invalid user id');
+   }
+
+   const deletedUser = await this.userModel.findByIdAndDelete(id).exec();
+
+    if (!deletedUser) {
+     throw new NotFoundException('User not found');
+   }
+
+   return deletedUser;
+ }
 
 
   async addWorkoutPlan(userId: string, workoutPlanId: string): Promise<User | null> {
